ef750208f5ae62f0ace4dab5bdc03f5a9b7215b0
extend0 <carterd17@gmail.com>
2021-06-18 16:48:31 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Cartesian3, Color } from "cesium";
+import { Ion, Cartesian3, Color } from "cesium";
 import { Viewer, Entity } from "resium";
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjOGM4ZTI2YS1jZDcwLTRmNWEtYTMwYS1iYmUyYTFlMjhkNGEiLCJpZCI6NTgwMDksInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjIzODk4MjE2LCJleHAiOjE2MjQ1MDMwMTZ9.du_rHveTKi1ZU5Q4070riMHuJ-sGxoDoQAOMwgSQJ6Y";
 
 const App = () => (
   <Viewer full>
